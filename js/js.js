396e847aa566ed0403541a78a161cb50f90b4626
renanpd95 <renandomiiciano95@gmail.com>
2024-05-19 12:00:11 -0300

    pagina artigo e dropdown criados

--- js/js.js
+++ js/js.js
@@ -35,5 +35,9 @@ window.addEventListener('scroll', function() {
       imagem.style.display = 'none'; // Oculta a imagem
     }
   });
-  
+
+
+//dropdown
+const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl)) 
 
